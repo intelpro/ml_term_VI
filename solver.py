d49a2348d4dff1d157e73e8939f2fe464c1cbd98
jhchris713 <49832475+jhchris713@users.noreply.github.com>
2019-06-19 21:22:41 +0900

    Update solver.py

--- solver.py
+++ solver.py
@@ -446,11 +446,17 @@ class Solver(object):
         cost = F.cross_entropy(h, y_true)
 
         # adversarial image classification
-        if targeted:
-            x_adv, h_adv, h = self.attack.i_fgsm(x, y_target, True, eps, alpha)
+        if iteration == 1:
+            if targeted:
+                x_adv, h_adv, h, adv_noise = self.attack.fgsm(x, y_target, True, eps)
+            else:
+                x_adv, h_adv, h, adv_noise = self.attack.fgsm(x, y_true, False, eps)
         else:
-            x_adv, h_adv, h = self.attack.i_fgsm(x, y_true, False, eps, alpha)
-
+            if targeted:
+                x_adv, h_adv, h = self.attack.i_fgsm(x, y_target, True, eps, alpha, iteration)
+            else:
+                x_adv, h_adv, h = self.attack.i_fgsm(x, y_true, False, eps, alpha, iteration)
+        
         prediction_adv = h_adv.max(1)[1]
         accuracy_adv = torch.eq(prediction_adv, y_true).float().mean()
         cost_adv = F.cross_entropy(h_adv, y_true)
